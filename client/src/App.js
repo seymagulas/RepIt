c6dbbd5135c21e269d2b05c3a68068b623b8db0b
Meera <t.meerarai@gmail.com>
2023-06-16 22:25:45 +0100

    feat: login and register pages were initialized

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
-import './App.css';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Logbook from './components/Logbook/Logbook';
 import Navbar from './components/Navbar/Navbar';
 import Workouts from './components/Workout-list/Workouts-list';
@@ -7,9 +6,12 @@ import MakeOrStartWorkout from './components/MakeOrStartWorkout/MakeOrStartWorko
 import ActiveWorkout from './components/Active-Workout/Active-Workout';
 import ContextProvider from './components/ContextProvider/ContextProvider';
 import { AppContext } from './components/ContextProvider/ContextProvider';
+import LoginForm from './components/Auth/LoginForm.tsx';
+import CreateAccountForm from './components/Auth/CreateAccountForm.tsx';
 import '@fortawesome/fontawesome-free/css/all.css';
-
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
@@ -21,25 +23,65 @@ function App() {
 
 function AppContent() {
   const { currentView } = useContext(AppContext);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [showCreateAccount, setShowCreateAccount] = useState(false);
+  const [showLoginForm, setShowLoginForm] = useState(true);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
+  const handleCreateAccountClick = () => {
+    setShowCreateAccount(true);
+    setShowLoginForm(false);
+  };
 
   return (
-    <div>
-      {currentView === 'logbook' && (
+    <div className={isLoggedIn ? 'logged-in' : ''}>
+      {isLoggedIn ? (
         <div>
-          <Logbook />
-          <Navbar />
+          {currentView === 'logbook' && (
+            <>
+              <Logbook />
+              <Navbar />
+            </>
+          )}
+          {currentView === 'workouts' && (
+            <>
+              <Workouts />
+              <Navbar />
+            </>
+          )}
+          {currentView === 'makeOrStart' && <MakeOrStartWorkout />}
+          {currentView === 'activeWorkout' && <ActiveWorkout />}
+          <button onClick={handleLogout}>Logout</button>
         </div>
+      ) : (
+        <>
+          {showLoginForm && (
+            <>
+            
+              <div className="loginContainer">
+                <LoginForm onLogin={handleLogin} onCreateAccountClick={handleCreateAccountClick} />
+                
+              </div>
+            </>
+          )}
+          {showCreateAccount && (
+            <>
+              <CreateAccountForm />
+            </>
+          )}
+        </>
       )}
-      {currentView === 'workouts' && (
-        <div>
-          <Workouts />
-          <Navbar />
-        </div>
-      )}
-      {currentView === 'makeOrStart' && <MakeOrStartWorkout />}
-      {currentView === 'activeWorkout' && <ActiveWorkout />}
     </div>
   );
 }
 
 export default App;
+
+
